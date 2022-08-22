62e1c360f8c1843f756b10025e4a73496b3cb0c2
Edwin shikuku <shikuku.ochieng@gmail.com>
2022-08-22 08:31:11 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+
+function scuberGreetingForFeet(randomValue){
   // Write your code here!
+ let result;
+ if (randomValue<=400){
+  result = 'This one is on me!';
+  return result;
+ }
+else if (randomValue>=2000 && randomValue<=2500){
+  result = 'I will gladly take your thirty bucks.';
+  return result;
+}
+else if (randomValue>2500){
+  result = 'No can do.';
+  return result;
+}
+  }
+
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  
+   return city === "NYC" ? "Ok, sounds good."  : "No go." ;
+ 
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+ 
+  switch (tip){ 
+    case "generous":
+      return 'Thank you so much.';
+          case "not as generous":
+      return 'Thank you.';
+          default:
+      return 'Bye.';
+      
+  }
+};
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
